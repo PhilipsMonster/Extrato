f6c0583e05039f50fc082dcedd7b7876d47dcfc8
Marcelo Gomes de Melo <marcelogo@cielo.com.br>
2020-06-24 20:23:34 -0300

    incluindo validador

--- src/main/java/extratolacamento/domain/Extrato.java
+++ src/main/java/extratolacamento/domain/Extrato.java
@@ -1,27 +1,62 @@
 package extratolacamento.domain;
 
+import org.springframework.context.annotation.Bean;
+
+import javax.persistence.Entity;
+
+@Entity
 public class Extrato {
-    private Long id;
-    private String nome;
+    private String firstName;
+    private String lastName;
+    private String street;
+    private String city;
+    private String state;
 
-    public Extrato(Long id, String nome) {
-        this.id = id;
-        this.nome = nome;
+    public Extrato(String firstName, String lastName, String street, String city, String state) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.street = street;
+        this.city = city;
+        this.state = state;
     }
 
-    public Long getId() {
-        return id;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public String getNome() {
-        return nome;
+    public String getLastName() {
+        return lastName;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public String getStreet() {
+        return street;
     }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public String getCity() {
+        return city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setState(String state) {
+        this.state = state;
     }
 }
--- src/main/java/extratolacamento/service/ExtratoService.java
+++ src/main/java/extratolacamento/service/ExtratoService.java
@@ -1,43 +1,53 @@
 package extratolacamento.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import extratolacamento.domain.Extrato;
+import org.json.simple.JSONArray;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
 @Service
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ExtratoService {
-
 
     public List<Extrato> getExtrato() throws IOException, ParseException {
 
-        //JSONParser jsonparser = new JSONParser();
+        JSONParser jsonparser = new JSONParser();
         ArrayList<Extrato> extratos = new ArrayList<>();
 
-        try {
+        try (FileReader reader = new FileReader("C:/Source/Extrato/src/main/resources/extrato.json"))
+        {
+            Object obj = jsonparser.parse(reader);
+            JSONObject jo = (JSONObject)obj;
 
-            //String fileName = "*/extrato";
-            //String filePath = fileName;
-            //Object obj = jsonparser.parse(new FileReader(filePath));
-            //JSONObject jsonObject = (JSONObject)obj;
+            // getting firstName and lastName
+            String firstName = (String) jo.get("firstName");
+            String lastName = (String) jo.get("lastName");
 
-            ObjectMapper mapper = new ObjectMapper();
-            InputStream is = getClass().getResourceAsStream("/extrato.json");
-            Object obj = mapper.readValue(is, ExtratoService.class);
-            JSONObject jsonObject = (JSONObject)obj;
+            JSONArray array = (JSONArray)jo.get("address");
+            String street = "";
+            String city = "";
+            String state = "";
 
-            String quantidadeLancamentos = (String)jsonObject.get("firstName");
-            String quantidadeRemessas = (String)jsonObject.get("lastName");
+            for (int i=0;i<array.size();i++)
+            {
+                JSONObject address = (JSONObject) array.get(i);
+                street = (String)address.get("street");
+                city = (String)address.get("city");
+                state = (String)address.get("state");
+            }
 
-            extratos.add(new Extrato(1L, quantidadeLancamentos));
-            extratos.add(new Extrato(2L, quantidadeRemessas));
+            extratos.add(new Extrato(firstName, lastName, street, city, state));
+
 
         }catch (Exception e) {
             e.printStackTrace();
