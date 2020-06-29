dd57be0dba1d71b0840a859ffb6a30aa309f5939
PhilipsMonster <philipsmonster@gmail.com>
2020-06-29 01:53:40 -0300

    alterando configuracoes seguranca

--- src/main/java/extratolacamento/security/SecurityConfig.java
+++ src/main/java/extratolacamento/security/SecurityConfig.java
@@ -10,10 +10,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfig  extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
+        super.configure(http);
     }
 }
